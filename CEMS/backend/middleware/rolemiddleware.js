eee320d9bf50e26a65fc63e11bd78ef9d3fb61c4
priyanka <priya2006@gmail.com>
2026-03-24 12:21:11 +0530

    merge aborted

---
+++ CEMS/backend/middleware/rolemiddleware.js
@@ -0,0 +1,15 @@
+function roleMiddleware(allowedRoles) {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "User not authenticated" });
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: "Access denied" });
+        }
+
+        next();
+    };
+}
+
+module.exports = roleMiddleware;
